aa669e5e3acc0db7b183748cf999882bd0f89e33
Mikael Titinovskiy <Mikael.Titinovskii@mindgeekconsultants.com>
2022-07-07 22:33:57 +0300

    Лекция про апиху, разные методы

--- src/Controller/CreditCardController.php
+++ src/Controller/CreditCardController.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CreditCardController extends AbstractController
 {
@@ -23,6 +25,27 @@ class CreditCardController extends AbstractController
         $this->creditCardRepository = $creditCardRepository;
     }
 
+    #[Route('/credit-card/{id}', name: 'get_one_cred', methods: 'GET')]
+    public function getOne(Request $request): Response
+    {
+        /// securty->check($user
+        $id = $request->get('id'); // 2
+
+        $card = $this->creditCardRepository->find($id); //null
+
+        if(!$card){
+            $response = new Response();
+            return $response->setStatusCode(404);
+        }
+
+        return $this->json([
+            'id'     => $card->getId(), // null->getId
+            'pin'    => $card->getPin(),
+            'number' => $card->getNumber(),
+        ]);
+    }
+
+
     #[Route('/credit-card', name: 'get_cret', methods: 'GET')]
     public function index(): JsonResponse
     {
@@ -32,8 +55,23 @@ class CreditCardController extends AbstractController
         ]);
     }
 
+
+    #[Route('/credit-card/{id}', name: 'delete_creditcard', methods: 'DELETE')]
+    public function delete(Request $request): JsonResponse
+    {
+        $id = $request->get('id');
+        $creditCard = $this->creditCardRepository->find($id);
+
+        if ($creditCard) {
+            $this->entityManager->remove($creditCard);
+            $this->entityManager->flush();
+        }
+
+        return $this->json([]);
+    }
+
     #[Route('/credit-card', name: 'post_cc', methods: 'POST')]
-    public function new(Request $request): JsonResponse
+    public function new(Request $request, ValidatorInterface $validator): Response
     {
         // получаем данные
         $number = $request->get('number');
@@ -43,6 +81,15 @@ class CreditCardController extends AbstractController
         $cc = new CreditCard();
         $cc->setNumber($number);
         $cc->setPin($pin);
+
+
+        $errors = $validator->validate($cc);
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+            $response = new Response();
+            $response->setContent($errorsString);
+            return $response->setStatusCode(400);
+        }
 
         // сохраняем карту
         $this->entityManager->persist($cc);
--- src/Entity/CreditCard.php
+++ src/Entity/CreditCard.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CreditCardRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CreditCardRepository::class)]
 class CreditCard
@@ -16,6 +17,16 @@ class CreditCard
     #[ORM\Column(type: 'string', length: 255)]
     private $number;
 
+    /**
+     * @Assert\NotBlank
+     * @Assert\Positive
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 3,
+     *      minMessage = "11111Your first name must be at least {{ limit }} characters long",
+     *      maxMessage = "22222Your first name cannot be longer than {{ limit }} characters"
+     * )
+     */
     #[ORM\Column(type: 'integer')]
     private $pin;
 
