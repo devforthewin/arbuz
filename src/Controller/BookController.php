69b20c84dce70aef99d9660a1fd9ba323a2cd197
Mikael Titinovskiy <Mikael.Titinovskii@mindgeekconsultants.com>
2022-09-05 12:19:02 +0300

    Лекция про кэш mutex сервисы

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\CreditCard;
 use App\Entity\Publisher;
 use App\Repository\BookRepository;
+use App\Service\KnigaService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,53 +17,104 @@ class BookController extends AbstractController
 {
     private BookRepository $bookRepository;
     private EntityManagerInterface $entityManager;
+    private KnigaService $knigaService;
 
-    public function __construct(BookRepository $bookRepository, EntityManagerInterface $entityManager){
+    public function __construct(
+        BookRepository $bookRepository,
+        EntityManagerInterface $entityManager,
+        KnigaService $knigaService
+    ) {
         $this->bookRepository = $bookRepository;
-        $this->entityManager = $entityManager;
+        $this->entityManager  = $entityManager;
+        $this->knigaService   = $knigaService;
+    }
+
+    #[Route('/book/create', name: 'cr_book')]
+    public function create(): JsonResponse
+    {
+        $this->entityManager->beginTransaction();
+        try {
+            $publisher = new Publisher();
+            $publisher->setName('Publisher'.rand());
+            $publisher->setAddress(rand());
+
+            $author = new Author();
+            $author->setName('Error');
+
+            $book = new Book();
+            $book->setName('book'.rand());
+            $book->addAuthor($author);
+            $book->setPublisher($publisher);
+
+            $this->entityManager->persist($author);
+            $this->entityManager->persist($book);
+            $this->entityManager->persist($publisher);
+            $this->entityManager->flush();
+            echo "my sozdali knigi";
+            throw new \Exception();
+
+            $credcard = new CreditCard();
+            $credcard->setPin(123);
+            $credcard->setNumber('11111111111111111');
+            $this->entityManager->persist($credcard);
+            $this->entityManager->flush();
+            echo "my sozdali cc";
+
+
+            $this->entityManager->commit();
+        } catch (\Throwable $e) {
+            echo "Rollback";
+            $this->entityManager->rollback();
+        }
+
+        return $this->json($this->knigaService->getBooks('Error'));
     }
 
     #[Route('/book/list', name: 'list_book')]
-    public function list():JsonResponse
+    public function list(): JsonResponse
     {
-        $books = $this->bookRepository->findAllByAuthor('Mihail');
+        $result = $this->knigaService->getBooks('Mihail');
 
-        $results = [];
-        foreach ($books as $book) {
-            $authors = $book->getAuthor();
-            $results[] = [
-                'name' => $book->getName(),
-                'author'=> $authors->get(0)->getName(),
-                'publisher' => $book->getPublisher()->getName()
-            ];
-        }
-
-        return $this->json($results);
+        return $this->json($result);
     }
 
-    #[Route('/new', name: 'app_book')]
+    #[Route('/new', methods: ['GET'], name: 'app_book')]
     public function index(): JsonResponse
     {
-        $publisher = new Publisher();
-        $publisher->setName('Publisher'. rand());
-        $publisher->setAddress(rand());
+        $this->entityManager->beginTransaction();
 
-        $author = new Author();
-        $author->setName('Navalny '.rand());
+        try {
+            $publisher = new Publisher();
+            $publisher->setName('Publisher'.rand());
+            $publisher->setAddress(rand());
 
-        $book = new Book();
-        $book->setName('book'. rand());
-        $book->addAuthor($author);
-        $book->setPublisher($publisher);
+            $author = new Author();
+            $author->setName('Navalny '.rand());
 
-        $this->entityManager->persist($publisher);
-        $this->entityManager->persist($author);
-        $this->entityManager->persist($book);
-        $this->entityManager->flush();
+            $book = new Book();
+            $book->setName('book'.rand());
+            $book->addAuthor($author);
+            $book->setPublisher($publisher);
+
+            $this->entityManager->persist($publisher);
+            $this->entityManager->persist($author);
+            $this->entityManager->persist($book);
+            $this->entityManager->flush();
+        } catch (\Throwable $e) {
+        }
+
+
+        /**
+         *    Throwable
+         *        Exception
+         *          InvalidArgumentException
+         */
 
 
         return $this->json([
-           'ok'
+            'ok',
         ]);
     }
+
+
 }
---
+++ src/Services/KnigaService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class KnigaService
+{
+    private BookRepository $bookRepository;
+    private \Symfony\Contracts\Cache\CacheInterface $cache;
+
+    public function __construct(
+        BookRepository $bookRepository,
+        EntityManagerInterface $entityManager,
+        \Symfony\Contracts\Cache\CacheInterface $cache
+
+    ){
+        $this->bookRepository = $bookRepository;
+        $this->entityManager = $entityManager;
+        $this->cache = $cache;
+    }
+
+    public function getBooks(string $author)
+    {
+        $result = $this->cache->get("list_book_$author", function (ItemInterface $item) use ($author) {
+            $item->expiresAfter(60 * 60 * 24);
+//            $item->tag(['books','list']);
+
+            $books = $this->bookRepository->findAllByAuthor($author);
+
+            $results = [];
+            foreach ($books as $book) {
+                $authors = $book->getAuthor();
+                $results[] = [
+                    'name' => $book->getName(),
+                    'author'=> $authors->get(0)->getName(),
+                    'publisher' => $book->getPublisher()->getName()
+                ];
+            }
+
+            return $results;
+        });
+
+
+        return $result;
+    }
+}
